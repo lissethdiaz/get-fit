3c3f74f6d029f39c9b6921991b321190b81e56f7
Liss Diaz <lissdiaz15@gmail.com>
2022-04-05 22:35:07 -0600

    navbar styled a bit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navbar from "./components/navbar"
-import ExercisesList from "./components/exerciseList";
-import EditExercise from "./components/editExercise";
-import CreateExercise from "./components/createExercise";
-import CreateUser from "./components/createUser";
+// import ExercisesList from "./components/exerciseList";
+// import EditExercise from "./components/editExercise";
+// import CreateExercise from "./components/createExercise";
+// import CreateUser from "./components/createUser";
 
 const App = () => {
   return (
@@ -14,10 +15,10 @@ const App = () => {
     <div className="container">
       <Navbar />
       <br/>
-      <Route path='/' exact component={ExercisesList} />
+      {/* <Route path='/' exact component={ExercisesList} />
       <Route path="/edit/:id" component={EditExercise} />
       <Route path="/create" component={CreateExercise} />
-      <Route path="/user" component={CreateUser} />
+      <Route path="/user" component={CreateUser} /> */}
     </div>
     </Router>
   );
