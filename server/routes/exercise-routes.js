d0bf8db3c95e6763a98e65175c4d7862b31b9411
Liss Diaz <lissdiaz15@gmail.com>
2022-04-05 20:09:25 -0600

    exercise crud routes complete

--- server/routes/exercise-routes.js
+++ server/routes/exercise-routes.js
@@ -10,8 +10,8 @@ router.route('/').get((req, res) => {
 router.route('/add').post((req, res) => {
   const username = req.body.username;
   const name = req.body.name;
-  const duration = req.body.duration;
-  const distance = req.body.distance;
+  const duration = Number(req.body.duration);
+  const distance = Number(req.body.distance);
   const date = Date.parse(req.body.date);
 
   const newExercise = new Exercise({
@@ -23,8 +23,36 @@ router.route('/add').post((req, res) => {
   });
 
   newExercise.save()
-  .then(() => res.json('Exercise added!'))
+  .then(() => res.json('Exercise has been added'))
   .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').get((req, res) => {
+  Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Exercise has been deleted'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+  Exercise.findById(req.params.id)
+    .then(exercise => {
+      exercise.username = req.body.username;
+      exercise.name = req.body.name;
+      exercise.duration = Number(req.body.duration);
+      exercise.distance = Number(req.body.distance);
+      exercise.date = Date.parse(req.body.date);
+
+      exercise.save()
+        .then(() => res.json('Exercise has been updated'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 module.exports = router;
